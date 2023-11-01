4bb80e3a52fb1c6e6d92e5f949d0cd5db027dc09
Christian Westgaard <comlock@gmail.com>
2023-11-01 07:28:35 +0100

    jquery and jquery-ui are pure peerDependencies

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
-// import 'jquery';
-import 'jquery-ui/ui/widgets/sortable'; // Required by slick.grid
+// These are peerDependencies, aka expected to exist the in global scope.
+// import 'jquery'; // slick.grid has code that checks for jQuery
+// import 'jquery-ui/ui/widgets/sortable'; // slick.grid has code that checks for $.fn.sortable
 
 // The newest version on npmjs.com is 2.2.2, 10 years ago, so I suspect we have a custom version of this
 // https://www.npmjs.com/package/jquery.event.drag?activeTab=versions
-import './lib/jquery.event.drag-2.3.js'; // Needed by slick.grid
+import './lib/jquery.event.drag-2.3.js'; // slick.grid has code that checks for jQuery.fn.drag
 
 // The newest version on npmjs.com is 2.2.2, 7 years ago, so I suspect we have a custom version of this
 // https://www.npmjs.com/package/jquery.event.drop?activeTab=versions
 import './lib/jquery.event.drop-2.3.js';
 
-import './slick.core.js'; // Needed by slick.grid
+import './slick.core.js'; // slick.grid has code that checks for Slick
 import './slick.grid.js'; // NOTE: Requires jquery-ui/ui/widgets/sortable
 import './slick.dataview.js';
 import './slick.remotemodel.js';
